4e8ab78e6f8f99d27fc37255f4fe61d218bf8c94
CristobalBenitezx <Cristobalbenitez.iccp@example.com>
2022-08-26 11:14:39 -0400

    subiendo contenido

---
+++ Benitez_Cristobal_JS_Jugueteo.js
@@ -0,0 +1,15 @@
+var alturaNiño = 53
+    function muestraSiElNiñoPuedeSubirALaMontañaRusa(alturaNiño)
+    {
+        if(alturaNiño > 53){
+        console.log('¡Súbete, chico!')
+
+    }
+     else 
+
+    {
+        console.log('Lo siento, chico. Tal vez el próximo año')
+    }
+
+    
+}
